8d8b381a0b29da8a8f20d36bb951be2faea8c3c9
Ganda <gandaszn@gmail.com>
2023-05-07 14:44:05 +0700

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Josua Hutapea on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
